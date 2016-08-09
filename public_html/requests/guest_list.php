0a61270358d35d534b0d15aa9d5e13d92d961bc7
benwhite10 <ben.white10@outlook.com>
2016-08-09 15:28:42 +0100

    Inital set up

---
+++ public_html/requests/guest_list.php
@@ -0,0 +1,7 @@
+<?php
+
+$include_path = '/var/Wedding';
+include_once $include_path . '/includes/db_functions.php';
+include_once $include_path . '/includes/session_functions.php';
+
+echo 'Hello';
